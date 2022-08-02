7198a029c1526b2652f0ad86a54696a4b74ac2d7
gheorghe.molnar <molnargheorghe@gmail.com>
2022-08-02 16:17:02 +0200

    truffle: update truffle config

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -48,6 +48,15 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraProjectId}`),
+      network_id: 4,       // Rinkeby's network id
+      // chain_id: 5,         // Rinkeby's chain id
+      gas: 5500000,        // Gas limit used for deploys.
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets)
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
