f791791ec8af9150331eae767bf519d0427aa066
gheorghe.molnar <molnargheorghe@gmail.com>
2022-08-04 12:50:21 +0200

    client: add route for events/:id

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { EthProvider } from "./contexts/EthContext";
 import Navbar from "./components/Navbar";
 import Listing from "./components/Listing";
 import FormEvent from "./components/FormEvent";
+import ViewEvent from "./components/ViewEvent";
 import FormTicket from "./components/FormTicket";
 import NoMatch from "./components/NoMatch";
 import "./App.css";
@@ -14,7 +15,13 @@ export default function App() {
       path: "/",
       element: <Layout />,
       children: [
-        { index: true, element: <Listing /> },
+        { 
+            index: true, element: <Listing />,
+        },
+        {
+          path: "/events/:id",
+          element: <ViewEvent />,
+        },
         {
           path: "/addevent",
           element: <FormEvent />,
