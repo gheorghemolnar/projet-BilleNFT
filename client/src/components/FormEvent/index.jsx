5689134fd946951a2cb3a1267f98d0eb08730116
FlorianFavre <florian.favre-fiurino@laposte.net>
2022-08-04 15:49:44 +0200

    Ajout evenement marche + navbar ok

--- client/src/components/FormEvent/index.jsx
+++ client/src/components/FormEvent/index.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { useForm } from "react-hook-form";
-// import useEth from "../../contexts/EthContext/useEth";
+import React, { useEffect, useState, useRef } from 'react';
+import { useForm, Controller } from "react-hook-form";
+import { useNavigate } from 'react-router-dom';
+import useEth from "../../contexts/EthContext/useEth";
 
 export default function FormEvent() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const navigate = useNavigate();
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { state: {accounts, contractBilleStore, contractBilleEvent, contractCommunication, eventsCreated, ticketsSold}} = useEth();
+  const onSubmit = async data => {
+    console.log(data);
+    await handleCreateEvent(data);
+  }
+
+
+  const handleCreateEvent = async(params) => {
+    const {dateEvent, nameEvent, symbol, description, uri, ticketSupply1, ticketSupply2, ticketSupply3} =params;
+       try{
+      const dateTimestamp = Number (new Date(dateEvent));
+      await contractBilleStore.methods.createEvent(dateTimestamp, nameEvent, symbol, description, uri, [ticketSupply1, ticketSupply2, ticketSupply3]).send({from: accounts[0]});
+      navigate('/');
+    }catch(err){
+        console.log("Erreur", err);
+    }    
+  }
+
+  
   /* const { state: { artifact, contract } } = useEth(); */
   return(
     <main>
@@ -17,22 +37,80 @@ export default function FormEvent() {
         </div>
       </section>
 
-      <form className="p-3 bg-light" onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+      <form className="p-3 bg-light" onSubmit={handleSubmit(onSubmit)} >
+
+        <table>          
+          <tbody>
+            <tr>
+              <td>
+                <label className="p-3 bg-light" htmlFor="name">Nom de l'événement</label>
+              </td>
+              <td>
+              <input defaultValue="Toto" {...register("nameEvent")}      />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="p-3 bg-light" htmlFor="date">Date de l'évènement</label>
+              </td>
+              <td>
+                <input defaultValue="2022-05-20" type="date" {...register("dateEvent")}      />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="p-3 bg-light" htmlFor="monnaie">Monnaie</label>
+              </td>
+              <td>
+                <input defaultValue="BNF" {...register("symbol")}      />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="p-3 bg-light" htmlFor="description">Description</label>
+              </td>
+              <td>
+                <input defaultValue="La legende" {...register("description")}      />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="p-3 bg-light" htmlFor="Uri :">Uri</label>
+              </td>
+              <td>
+                <input defaultValue="181818222" {...register("uri")}      />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="p-3 bg-light" htmlFor="ticketSupplies">Nombre de tickets Pelouse</label>
+              </td>
+              <td>
+                <input type="number" defaultValue={50} {...register("ticketSupply1")}      />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="p-3 bg-light" htmlFor="ticketSupplies">Nombre de tickets Gradin</label>
+              </td>
+              <td>
+                <input type="number" defaultValue={30} {...register("ticketSupply2")}      />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="p-3 bg-light" htmlFor="ticketSupplies">Nombre de tickets VIP</label>
+              </td>
+              <td>
+                <input type="number" defaultValue={10} {...register("ticketSupply3")}      />
+              </td>
+            </tr>
+            </tbody>
+        </table>
+
+        <button  className="btn btn-primary" type="submit" >Envoyer</button>
       </form>
     </main>
   );
+  
 }
