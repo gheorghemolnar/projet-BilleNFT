46f08e46d5c773a7bf530493ded095d4429fc51a
gheorghe.molnar <molnargheorghe@gmail.com>
2022-08-05 13:48:49 +0200

    client: add utils for date, contracts

---
+++ client/src/utils/index.js
@@ -0,0 +1,22 @@
+import BilleEventABI from "../contracts/BilleEvent.json";
+import CommunicationABI from "../contracts/Communication.json";
+
+export const getContractStoreByAddress = (web3, abi, address) => {
+    const contract = new web3.eth.Contract(abi, address, {gasPrice: '20000000000'});
+    return contract;
+}
+
+export const getContractEventByAddress = (web3, address) => {
+    const contract = new web3.eth.Contract(BilleEventABI.abi, address, {gasPrice: '20000000000'});
+    return contract;
+}
+
+export const getContractCommByAddress = (web3, address) => {
+    const contract = new web3.eth.Contract(CommunicationABI.abi, address);
+    return contract;
+}
+
+export const getDateFromTimestamp = (date) => {
+    const dateEvent = Intl.DateTimeFormat('fr-FR', { year: 'numeric', month: '2-digit', day: '2-digit' }).format(date);
+    return dateEvent;
+}
