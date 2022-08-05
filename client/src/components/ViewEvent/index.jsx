af6c637ad4e183b2ba35c510a6c2ce6ba1a7f9cc
gheorghe.molnar <molnargheorghe@gmail.com>
2022-08-05 20:28:50 +0200

    client: view event

--- client/src/components/ViewEvent/index.jsx
+++ client/src/components/ViewEvent/index.jsx
@@ -1,21 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from "react-router-dom";
 
+import { getContractEventByAddress } from "../../utils";
 import useEth from "../../contexts/EthContext/useEth";
-import { getDateFromTimestamp } from "../../utils";
+import { getDateFromTimestamp, TICKET_CATEGORIES, TICKET_CATEGORIES_LABELS } from "../../utils";
 
 export default function ViewEvent() {
-  const { state: { eventsCreated = [] } } = useEth();
-  const {id} = useParams();
-  let eventInfos = { date: '', description: '', eventAddress: '', name: '', uri: '' };
-  const [currentEvent] = eventsCreated.filter(({returnValues: {eventAddress}}) => eventAddress === id);
+  const { state: { web3, accounts, ticketsSold = [], eventsCreated = [] } } = useEth();
+  const [contractBilleEvent, setContractBilleEvent] = useState(null);
+  const [stats, setStats] = useState( { balance: '', ticketsStats: [] });
+
+  const { id } = useParams();
+  const showEventStats = contractBilleEvent;
+
+  useEffect(() => {
+    if (web3 && (!contractBilleEvent)) {
+      const contractEvent = getContractEventByAddress(web3, id);
+      setContractBilleEvent(contractEvent);
+    }
+  }, [web3]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const [ balance, ticketsStats ] = await Promise.all([
+        contractBilleEvent.methods.getBalance().call({ from: accounts[0]}),
+        contractBilleEvent.methods.getEventStats().call({ from: accounts[0] })
+      ]);
+     
+      setStats ({ ...stats, balance: `${web3.utils.fromWei(balance, 'ether')} Eth`, ticketsStats });
+    }
+
+    if (contractBilleEvent) {
+      fetchData();
+    }
+  }, [contractBilleEvent]);
+
+  let eventInfos = { date: '', description: '', eventAddress: '', name: '', uri: '' }, eventStats, eventStatsTickets;
+  const [currentEvent] = eventsCreated.filter(({ returnValues: { eventAddress } }) => eventAddress === id);
 
   if (currentEvent) {
-    const {returnValues: { date, description, eventAddress, name, uri }} = currentEvent;
-    eventInfos = {date: getDateFromTimestamp(date), description, eventAddress, name, uri};
+    const { returnValues: { date, description, eventAddress, name, uri } } = currentEvent;
+    eventInfos = { date: getDateFromTimestamp(date), description, eventAddress, name, uri };
+    
+    // Stats sales / supply by Ticket category
+    eventStatsTickets = TICKET_CATEGORIES.map(i => {
+        return (<div key={i} className="row mb-3">
+          <label htmlFor="colFormLabel" className="col-sm-4 col-form-label">Tickets {`${TICKET_CATEGORIES_LABELS[i]}`}</label>
+          <div className="col-sm-8">
+            <input type="email" className="form-control  text-end" id="colFormLabel" placeholder="col-form-label" value={`${stats.ticketsStats[i]} / ${stats.ticketsStats[i+3]}`} disabled />
+          </div>
+        </div>
+      );
+    })
+
+    // Stats balance
+    eventStats = <section className="py-5 text-center">
+      <h4 className="fw-light">Admin - Event stats</h4>
+      <div className="row mb-3">
+          <label htmlFor="colFormLabelSm" className="col-sm-4 col-form-label col-form-label">Balance</label>
+          <div className="col-sm-8">
+            <input type="email" className="form-control  text-end" id="colFormLabelSm" placeholder="col-form-label" value={stats.balance} disabled />
+          </div>
+      </div>
+      {eventStatsTickets}
+    </section>;
   }
 
-return (
+  return (
     <main>
       <section className="py-5 text-center container">
         <div className="row py-lg-5">
@@ -28,7 +79,7 @@ return (
         </div>
       </section>
 
-      <div className="event-view p-3 bg-light">
+      <div className="event-view container p-3 bg-light">
         <div className="row mb-3">
           <label htmlFor="colFormLabelSm" className="col-sm-2 col-form-label col-form-label">Date</label>
           <div className="col-sm-10">
@@ -38,7 +89,7 @@ return (
         <div className="row mb-3">
           <label htmlFor="colFormLabel" className="col-sm-2 col-form-label">Name</label>
           <div className="col-sm-10">
-            <input type="email" className="form-control" id="colFormLabel" placeholder="col-form-label" value={eventInfos.name} disabled/>
+            <input type="email" className="form-control" id="colFormLabel" placeholder="col-form-label" value={eventInfos.name} disabled />
           </div>
         </div>
         <div className="row">
@@ -48,6 +99,9 @@ return (
           </div>
         </div>
       </div>
+      {
+        showEventStats && eventStats
+      }
 
       <section className="py-5 text-center container">
         <div className="row py-lg-5">
