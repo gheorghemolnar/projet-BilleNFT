9eda4e05c45dcb83d9fb05a4de9348d12038ca18
gheorghe.molnar <molnargheorghe@gmail.com>
2022-08-07 14:40:42 +0200

    client: add ticket price & amount formatter

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,9 @@
 import BilleEventABI from "../contracts/BilleEvent.json";
 import CommunicationABI from "../contracts/Communication.json";
 
+
+export const TICKET_PRICE = 0.02;
+
 export const TICKET_CATEGORIES = [0, 1, 2];
 export const TICKET_CATEGORIES_LABELS = ['Fosse', 'Gradins', 'VIP'];
 
@@ -34,6 +37,9 @@ export const getDateFromTimestamp = (date) => {
     const dateEvent = Intl.DateTimeFormat('fr-FR', { year: 'numeric', month: '2-digit', day: '2-digit' }).format(date);
     return dateEvent;
 }
+
+// Conversions
+export const formatAmount = (amount, web3, unit = 'ether') => web3 ? `${web3.utils.fromWei(amount, unit)} Eth`: '0 Eth';
 
 // Is App Owner
 export const isOwner = (currentUser, owner) => currentUser && currentUser.length > 0 ? currentUser[0] === owner : false;
