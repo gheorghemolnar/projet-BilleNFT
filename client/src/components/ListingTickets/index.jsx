770ef1a8704dfeb5bb6b398799fe7a04c1ba466b
gheorghe.molnar <molnargheorghe@gmail.com>
2022-08-07 23:42:25 +0200

    client: add Listing Tcikets

--- client/src/components/ListingTickets/index.jsx
+++ client/src/components/ListingTickets/index.jsx
@@ -4,14 +4,22 @@ import { Link } from "react-router-dom";
 import NoticeNoArtifact from "../NoticeNoArtifact";
 import NoticeWrongNetwork from "../NoticeWrongNetwork";
 import useEth from "../../contexts/EthContext/useEth";
-import { getDateFromTimestamp, isOwner } from "../../utils";
+import { getDateFromTimestamp, TICKET_CATEGORIES_LABELS } from "../../utils";
 
 export default function ListingTickets() {
-  const { state: { artifact, contractBilleEvent, owner, ticketsSold  } } = useEth();
-  console.log(`🚀 > ListingTickets ~ ticketsSold`, ticketsSold)
-  console.log(`🚀 > ListingTickets ~ owner`, owner)
+  const { state: { artifact, contractBilleEvent, owner, eventsCreated, ticketsSold  } } = useEth();
+  const myTickets = ticketsSold.filter (({returnValues} ) => returnValues.owner === owner);
+  const myEventIds = myTickets.map(({returnValues}) => returnValues.idEvent);
+  const myEventsInfos = eventsCreated.reduce((acc, {returnValues}) => {
+    if(myEventIds.includes(returnValues.id)) {
+      acc[returnValues.id] = {
+        ...returnValues
+      }
+    }
+    return acc;
+  }, {});
 
-  const eventHeader = <section key="header" className="py-5 text-center container">
+  const myTicketsHeader = <section key="header" className="py-5 text-center container">
       <div className="row py-lg-5">
         <div className="col-lg-6 col-md-8 mx-auto">
           <h1 className="fw-light">Mes tickets</h1>
@@ -19,35 +27,34 @@ export default function ListingTickets() {
       </div>
     </section>;
   
-  // const myTicketsList =
-  //   ticketsSold.map(({ returnValues }) => {
-  //     const { eventAddress, name, description, date, uri } = returnValues;
-  //     const dateEve = getDateFromTimestamp(date);
-  //     const imageSrc = uri !== '' ? `${uri.split('//')[1]}` : '';
+  const myTicketsList =
+    myTickets.map(({ returnValues: { category, idEvent,idTicket,quantity } }) => {
+      const { eventAddress, name, description, date, uri } = myEventsInfos[idEvent];
+      const dateEve = getDateFromTimestamp(date);
+      const imageSrc = uri !== '' ? `${uri.split('//')[1]}` : '';
 
-  //     console.log(`🚀  imageSrc`, imageSrc);
+      console.log(`🚀  imageSrc`, imageSrc);
 
-  //     return <div className="col-lg-6 p-3" key={returnValues.id}>
-  //       <div className="card shadow-sm">
-  //         {/* {
-  //           imageSrc ? <img src={`https://gateway.pinata.cloud/ipfs/${uri.split('//')[1]}`} alt="eventImage"/> : <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-  //         } */}
+      return <div className="col-lg-6 p-3" key={idTicket}>
+        <div className="card shadow-sm w-50">
+          {
+            imageSrc ? <img src={`https://gateway.pinata.cloud/ipfs/${uri.split('//')[1]}`} alt="eventImage"/> : <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          }
 
-  //         <div className="card-body">
+          <div className="card-body">
 
-  //           <Link to={`/events/${eventAddress}`} >
-
-  //             <p className="card-text">  {name} </p>
-  //             <p className="card-text"> {description}</p>
-  //             <div className="d-flex justify-content-between align-items-center">
-  //               <small className="text-muted">{dateEve}</small>
-  //             </div>
-  //           </Link>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   });
-  const eventContent = ["eventHeader", "eventList" ];
+            <Link to={`/events/${eventAddress}`} >
+              <p className="card-text"> <strong>{name}</strong></p>
+              <p className="card-text"> {description}</p>
+              <p className="card-text"> <strong>Date:</strong> {dateEve}</p>
+              <p className="card-text"> <strong>Nombre de tickets:</strong> {quantity}</p>
+              <p className="card-text"> <strong>Catégorie</strong>: {TICKET_CATEGORIES_LABELS[category]}</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    });
+  const eventContent = [myTicketsHeader, myTicketsList ];
 
   return (
     <main>
